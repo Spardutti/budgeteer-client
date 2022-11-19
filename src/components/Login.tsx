0d25c1aba8b804b2487e24589adca4017bf421e5
Spardutti <dspagnuolo@itcrowdarg.com>
2022-11-19 17:57:39 -0300

    errors re rendering

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,30 +1,31 @@
 import { apiManager } from "api";
+import axios from "axios";
 import { useState } from "react";
 
 export const Login = () => {
-	interface LoginErrors {
-		errors: { username?: string[]; password?: string[] };
-	}
-	const [loginErrors, setLoginErrors] = useState<LoginErrors>();
+	const [loginErrors, setLoginErrors] = useState();
 	const { mutateAsync, error, isLoading } = apiManager.useCreateUser("Churu", "");
 
 	const create = async () => {
 		try {
 			const x = mutateAsync();
-		} catch (error) {
-			console.log("ERR", error);
-			throw error;
-		}
+		} catch (error) {}
 	};
 
 	if (error) {
-		return <p>errors</p>;
+		console.log("ERR", error);
+		if (axios.isAxiosError(error)) {
+			const data = error.response?.data;
+			setLoginErrors(data);
+		}
 	}
+
 	return (
 		<>
 			<>
 				<div>Home</div>
 				<button onClick={create}>click</button>
+				{loginErrors ? loginErrors["username"] : null}
 			</>
 		</>
 	);
