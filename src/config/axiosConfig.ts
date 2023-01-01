ffa95a1622d2b15e2966cf81971308fd7b7f737e
Spardutti <luisdamian.sp@gmail.com>
2023-01-01 18:51:07 -0300

    server url

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const instance = axios.create({
-	baseURL: "http://localhost:5000",
+	//TODO set this dinamic with env
+	baseURL: "https://budgeteer-dockerless.onrender.com",
+	// baseURL: "http://localhost:5000",
 	headers: {
 		"Content-Type": "application/json",
 	},
